ff70a968b54573f209310d240c62e908d6889d27
mk3250d07295y@aol.com <Yoika>
2022-05-11 21:41:20 +0900

    update ruby memo command

--- shortcut/memo.rb
+++ shortcut/memo.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
-File.new(Dir.home.concat('/Desktop/memo.md'), 'a')
+file_name = ARGV[0].nil? ? 'memo' : ARGV[0]
+extension = ARGV[1].nil? ? 'md' : ARGV[1]
+path = "#{Dir.home}/Desktop/#{file_name}.#{extension}"
+
+File.new(path, 'a')
+system("open #{path}")
