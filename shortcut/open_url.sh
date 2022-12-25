a214f2db2ce545dc6eab5197c340f3ccf796a051
mk3250d07295y@aol.com <Yoika>
2022-12-25 20:46:29 +0900

    add syntax rust open url

---
+++ shortcut/open_url.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+option=$1
+
+cd $HOME/Public/workspace/Rust/open_browser
+
+if [ $option == "-l" ]; then
+    cargo run --release -q -- list
+elif [ $option == "-a"  ]; then
+    cargo run --release -q -- add ${@:2}
+elif [ $option == "-d"  ]; then
+    cargo run --release -q -- delete ${@:2}
+elif [ $option == "-b"  ]; then
+    cargo run --release -q -- brow ${@:2}
+else
+    cargo run --release -q -- brow ${@:2}
+fi
+
+cd - 1> /dev/null
